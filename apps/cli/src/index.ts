cb050f251b68f9ae72722048c80596f6214e1a22
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2023-10-16 10:30:49 +0100

    feat(cli): Add 'create' command

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,9 +1,21 @@
-console.log(`
- ____  __.__                      
-|    |/ _|  | _____ ___  __ ____  
-|      < |  | \\__  \\\\  \\/ // __ \\ 
-|    |  \\|  |__/ __ \\\\   /\\  ___/ 
-|____|__ \\____(____  /\\_/  \\___  >
-        \\/         \\/          \\/ 
-`);
-console.log('Welcome to Klave CLI!');
+import yargs from 'yargs';
+import figlet from 'figlet';
+import chalk from 'chalk';
+import { hideBin } from 'yargs/helpers';
+import { KLAVE_CYAN } from './lib/constants';
+
+const usage = 'klave [command] [...flags]';
+
+console.log(figlet.textSync('Klave'));
+console.log(KLAVE_CYAN(chalk.bold('Welcome to Klave CLI!')));
+console.log();
+
+yargs(hideBin(process.argv))
+    .usage(usage)
+    .commandDir('commands')
+    .alias({ h: 'help' })
+    .alias({ v: 'version' })
+    .demandCommand()
+    .help()
+    .argv;
+
---
+++ apps/cli/src/lib/constants.ts
@@ -0,0 +1,13 @@
+import chalk from 'chalk';
+
+export const KLAVE_CYAN = chalk.hex('#00FFD5');
+
+export const KLAVE_DARK_BLUE = chalk.hex('#00021A');
+
+export const KLAVE_LIGHT_BLUE = chalk.hex('#00BFFF');
+
+export const DOCS_URL = 'https://klave.com/docs';
+
+export const KLAVE_URL = 'https://klave.com';
+
+export const KLAVE_JSON = 'klave.json';
